703734c02535dd63656d998ceeb0cf22f3febf65
Mohamed Salman <164924354+Salman16143@users.noreply.github.com>
2025-11-12 15:06:52 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// ---
+const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
+const smallMenu = document.querySelector('.header__sm-menu')
+const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
+const headerHamMenuCloseBtn = document.querySelector(
+  '.header__main-ham-menu-close'
+)
+const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
+
+hamMenuBtn.addEventListener('click', () => {
+  if (smallMenu.classList.contains('header__sm-menu--active')) {
+    smallMenu.classList.remove('header__sm-menu--active')
+  } else {
+    smallMenu.classList.add('header__sm-menu--active')
+  }
+  if (headerHamMenuBtn.classList.contains('d-none')) {
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  } else {
+    headerHamMenuBtn.classList.add('d-none')
+    headerHamMenuCloseBtn.classList.remove('d-none')
+  }
+})
+
+for (let i = 0; i < headerSmallMenuLinks.length; i++) {
+  headerSmallMenuLinks[i].addEventListener('click', () => {
+    smallMenu.classList.remove('header__sm-menu--active')
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  })
+}
+
+// ---
+const headerLogoConatiner = document.querySelector('.header__logo-container')
+
+headerLogoConatiner.addEventListener('click', () => {
+  location.href = 'index.html'
+})
+
+let currentIndex = 0;
+const slidesWrapper = document.getElementById('slidesWrapper');
+const slides = document.querySelectorAll('.mySlide');
+const totalSlides = slides.length;
+
+function updateSlidePosition() {
+  slidesWrapper.style.transform = 'translateX(' + (-currentIndex * 100) + '%)';
+}
+
+function moveSlide(direction) {
+  currentIndex += direction;
+  if(currentIndex < 0) currentIndex = totalSlides - 1;
+  if(currentIndex >= totalSlides) currentIndex = 0;
+  updateSlidePosition();
+}
+
+updateSlidePosition(); // Initial call
